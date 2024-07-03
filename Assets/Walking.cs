2e33dfb2d9c21c95c5cefa926f6b79e4e0ea5d90
HyoketsuSenpai <Youseftela@gmail.com>
2024-07-03 20:32:50 +0300

    added bedroom,fixed some bugs, can go between the two rooms
    or maaybe broke everything idk.
    Try it for me

--- Assets/Walking.cs
+++ Assets/Walking.cs
@@ -6,6 +6,7 @@ using UnityEngine.UIElements;
 public class Walking : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public float rate = 1;
     void OnColliderStay (Collider other)
     {
         rb.velocity = Vector3.zero;
@@ -19,13 +20,13 @@ public class Walking : MonoBehaviour
     void move()
     {
         if(Input.GetKey(KeyCode.UpArrow))
-        {rb.velocity = Vector3.up * 1000  * Time.deltaTime;}
+        {rb.velocity = Vector3.up * 1000  * Time.deltaTime * rate;}
         else if(Input.GetKey(KeyCode.DownArrow))
-        {rb.velocity = Vector3.down * 1000  * Time.deltaTime;}
+        {rb.velocity = Vector3.down * 1000  * Time.deltaTime * rate;}
         else if(Input.GetKey(KeyCode.LeftArrow))
-        {rb.velocity = Vector3.left * 1000  * Time.deltaTime;}
+        {rb.velocity = Vector3.left * 1000  * Time.deltaTime * rate;}
         else if(Input.GetKey(KeyCode.RightArrow))
-        {rb.velocity = Vector3.right * 1000 * Time.deltaTime;}
+        {rb.velocity = Vector3.right * 1000 * Time.deltaTime * rate;}
         else
         {rb.velocity = Vector3.zero;}
     }
